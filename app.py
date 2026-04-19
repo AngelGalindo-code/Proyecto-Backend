19fcef501b7ceaa419ca88824ed0cd3b92eba048
Angel Galindo <agalindo@fi.uba.ar>
2026-04-19 19:45:44 -0300

    Registro el Blueprint de partidos en app.py para activar los endpoints

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
+from routes.partidos import partidos_bp
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "¡Hola mundo!" 
+app.register_blueprint(partidos_bp)
 
-# Configuramos el servidor Web para que se ejecute en el puerto 8080
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
