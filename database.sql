f582c24d0a00c55f00ff4019de8481570055fdd2
Angel Galindo <agalindo@fi.uba.ar>
2026-04-19 11:38:36 -0300

    agrego tablas de database.sql

---
+++ database.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE ProDe;
+USE ProDe;
+
+CREATE TABLE partidos (
+	id_partido INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    estadio VARCHAR(50),
+    ciudad VARCHAR(50),
+    equipo_local VARCHAR(50) NOT NULL,
+    equipo_visitante VARCHAR(50) NOT NULL,
+    fecha DATE NOT NULL,
+    fase VARCHAR(50)
+    );
+
+CREATE TABLE usuarios (
+	id_usuario INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    usuario VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    puntos INT
+	);
+    
+CREATE TABLE resultados (
+	id_partido INT NOT NULL,
+    goles_local INT NOT NULL,
+    goles_visitante INT NOT NULL,
+    FOREIGN KEY (id_partido) REFERENCES partidos(id_partido)
+	);
+
+CREATE TABLE predicciones (
+	id_usuario INT NOT NULL,
+    id_partido INT NOT NULL,
+    goles_local INT NOT NULL,
+    goles_visitante INT NOT NULL,
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY(id_partido) REFERENCES partidos(id_partido)
+    );
+
+
